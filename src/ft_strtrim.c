8a4464a44ea99b538e97204f00b44aa0d31b7c34
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-04-29 18:22:38 +0200

    Implement ft_strtrim.c

--- src/ft_strtrim.c
+++ src/ft_strtrim.c
@@ -1,4 +1,39 @@
+#include <stdlib.h>
+
+static int	is_in_set(char c, const char *set)
+{
+	while (*set)
+	{
+		if (c == *set)
+			return (1);
+		set++;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(const char *s1, const char *set)
 {
+	size_t	start;
+	size_t	end;
+	char	*str;
+	size_t	i;
 
+	if (!s1 || !set)
+		return (NULL);
+	start = 0;
+	while (s1[start] && is_in_set(s1[start], set))
+		start++;
+	end = 0;
+	while (s1[end])
+		end++;
+	while (end > start && is_in_set(s1[end - 1], set))
+		end--;
+	str = (char *)malloc(sizeof(*s1) * (end - start + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (start < end)
+		str[i++] = s1[start++];
+	str[i] = '\0';
+	return (str);
 }
