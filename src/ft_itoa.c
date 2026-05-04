22928b9df4e7aecfc245e25b9d712aa9a30f84fe
3ka1tz <ekaitzlopez2025@gmail.com>
2026-05-04 11:39:24 +0200

    Update ft_itoa

--- src/ft_itoa.c
+++ src/ft_itoa.c
@@ -1,9 +1,8 @@
-#include <stddef.h>
 #include <stdlib.h>
 
 #include "../include/libft.h"
 
-static size_t	ft_numlen(int n)
+static size_t	numlen(int n)
 {
 	size_t	len;
 
@@ -20,12 +19,12 @@ static size_t	ft_numlen(int n)
 
 char	*ft_itoa(int n)
 {
-	size_t	len;
 	char	*s;
+	size_t	len;
 
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
-	len = ft_numlen(n);
+	len = numlen(n);
 	s = malloc(len + 1);
 	if (!s)
 		return (NULL);
@@ -37,10 +36,11 @@ char	*ft_itoa(int n)
 	}
 	if (n == 0)
 		s[0] = '0';
-	if (n > 0)
+	while (n > 0)
 	{
-		s[--len] = (n % 10) + '0';
+		s[len - 1] = (n % 10) + '0';
 		n /= 10;
+		len--;
 	}
 	return (s);
 }
