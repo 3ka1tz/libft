818087fc957cd5a8b61578614c9bf53ce1e6df48
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-05-04 17:55:10 +0200

    Update ft_memcmp.c

--- src/ft_memcmp.c
+++ src/ft_memcmp.c
@@ -2,18 +2,17 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*p1;
-	const unsigned char	*p2;
-	size_t				i;
+	const unsigned char	*ptr1;
+	const unsigned char	*ptr2;
 
-	p1 = (const unsigned char *)s1;
-	p2 = (const unsigned char *)s2;
-	i = 0;
-	while (i < n)
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+	while (n--)
 	{
-		if (p1[i] != p2[i])
-			return (p1[i] - p2[i]);
-		i++;
+		if (*ptr1 != *ptr2)
+			return (*ptr1 - *ptr2);
+		ptr1++;
+		ptr2++;
 	}
 	return (0);
 }
