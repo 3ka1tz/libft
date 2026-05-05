bce001eb293ae242fdab5e3037296a837731476b
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-05-05 18:37:33 +0200

    Update ft_lstnew

--- src/ft_lstnew.c
+++ src/ft_lstnew.c
@@ -1,15 +1,15 @@
-#include "libft.h"
-
 #include <stdlib.h>
+
+#include "libft.h"
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*new;
+	t_list	*new_node;
 
-	new = malloc(sizeof(t_list));
-	if (!new)
+	new_node = malloc(sizeof(t_list));
+	if (!new_node)
 		return (NULL);
-	new->content = content;
-	new->next = NULL;
-	return (new);
+	new_node->content = content;
+	new_node->next = NULL;
+	return (new_node);
 }
