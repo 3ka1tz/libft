1f666880a7306388cefc87b49cb99631b3fe1b7e
Ekaitz López <ekaitzlopez2025@gmail.com>
2026-05-05 18:44:01 +0200

    Update ft_strjoin

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -1,30 +1,30 @@
-#include "libft.h"
-
 #include <stdlib.h>
+
+#include "libft.h"
 
 char	*ft_strjoin(const char *s1, const char *s2)
 {
-	char	*strjoin;
+	char	*join;
 	size_t	i;
 	size_t	j;
 
 	if (!s1 || !s2)
 		return (NULL);
-	strjoin = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!strjoin)
+	join = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!join)
 		return (NULL);
 	i = 0;
 	while (s1[i])
 	{
-		strjoin[i] = s1[i];
+		join[i] = s1[i];
 		i++;
 	}
 	j = 0;
 	while (s2[j])
 	{
-		strjoin[i + j] = s2[j];
+		join[i + j] = s2[j];
 		j++;
 	}
-	strjoin[i + j] = '\0';
-	return (strjoin);
+	join[i + j] = '\0';
+	return (join);
 }
